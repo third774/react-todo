70ff44caf590b149e25f219b4be5f9c1845c935b
Kevin Kipp <kevin.kipp@gmail.com>
2017-05-27 09:10:25 -0500

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,8 +1,11 @@
+var uuid = require('uuid');
+var moment = require('moment');
+
 
 export function searchText(state = '', action) {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
-      return action.searchText; 
+      return action.searchText;
     default:
       return state;
   }
@@ -12,6 +15,36 @@ export function showCompleted(state = false, action) {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
+    default:
+      return state;
+  }
+}
+
+export function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+            completedAt: !todo.completed ? moment().unix() : undefined
+          };
+        } else {
+          return todo;
+        }
+      });
     default:
       return state;
   }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,54 @@ describe('reducers', () => {
     });
   });
 
+  describe('todos reducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Go to the store'
+      };
+      var res = reducers.todos(df([]), df(action));
+      expect(res.length).toBe(1);
+      expect(res[0].text).toBe(action.text);
+    });
+
+    it('should set completed boolean true and update completedAt time if marking completed', () => {
+      var todo = {
+        id: 'aaowejfoaiwejfoiawj',
+        text: 'Walk the dog',
+        completed: false,
+        createdAt: 1123,
+        completedAt: undefined
+      };
+      var todosState = [todo];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: todo.id
+      };
+      var res = reducers.todos(df(todosState), df(action));
+      expect(res.length).toBe(todosState.length);
+      expect(res[0].completed).toBe(true);
+      expect(isNaN(res[0].completedAt)).toBe(false);
+    });
+
+    it('should set completed boolean false and completedAt undefined if marking not completed', () => {
+      var todo = {
+        id: 'aaowejfoaiwejfoiawj',
+        text: 'Walk the dog',
+        completed: true,
+        createdAt: 1123,
+        completedAt: 1231231
+      };
+      var todosState = [todo];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: todo.id
+      };
+      var res = reducers.todos(df(todosState), df(action));
+      expect(res.length).toBe(todosState.length);
+      expect(res[0].completed).toBe(false);
+      expect(isNaN(res[0].completedAt)).toBe(true);
+    });
+  })
+
 });
